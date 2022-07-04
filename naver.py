8e00a30b5aa0a513a2097b3cfb157fd5583ee68b
walrus22 <104523755+walrus22@users.noreply.github.com>
2022-07-05 01:56:27 +0900

    7.4.final

--- naver.py
+++ naver.py
@@ -16,6 +16,7 @@ import sys
 # except Exception:
 #       print(Exception)
 #     # print(traceback.format_exc())
+start = time.time()
 
 chrome_options = Options()
 chrome_options.add_argument("--incognito")
@@ -24,8 +25,6 @@ driver = webdriver.Chrome(chrome_options=chrome_options, executable_path="C:/Win
 
 url = "https://comic.naver.com/webtoon/weekday"
 
-
-src_dict = {}
 day_list = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
 driver.get(url)
 time.sleep(2)
@@ -33,10 +32,10 @@ test_list = {}
 test = driver.find_elements_by_css_selector(".thumb")
 
 
-for i in range(10):
+for i in range(len(test)):
     str_temp = test[i].find_element_by_xpath("following-sibling::a").get_attribute("href")
-    id_temp = str_temp[str_temp.index("titleId=")+8 : str_temp.index("&")] 
-    day_temp = str_temp[str_temp.index("weekday=")+8 : ]
+    id_temp = str_temp[str_temp.index("titleId=") + 8 : str_temp.index("&")] 
+    day_temp = str_temp[str_temp.index("weekday=") + 8 : ]
     title_temp = test[i].find_element_by_xpath("following-sibling::a").get_attribute("title")
     test_list[id_temp] = []
     test_list[id_temp].append(title_temp)
@@ -44,21 +43,19 @@ for i in range(10):
     
 print("######################################################################")   
 print(test_list)
+print("######################################################################")   
+print("time :", time.time() - start)   
     
-    
-# find_element return값이 뭐임? 
-# 이게 부모 추적이 가능한가?
-
 
 """
-TODO: ㅎㅎ
  1. 요일별 class = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
  2. li or class=thumb 순서대로 탐색, 순위 할당 
  3. href로 titleId 태깅, ID 리스트 저장  (*https://comic.naver.com/webtoon/list?titleId=758037 (titleId로 접근가능)
  4. class="title"저장하든지 아니면 링크탔을때 저장
  5. 날짜 끝나면? or 그냥 일욜까지 추적 다하고?
+ 
+ TODO:
  6. 링크타고 들어감
  7. 일단 최신화 정보 & 별점 & 하트수 & 시놉시스 등 딕셔너리 저장
  8. class="cnt_page"로 다음페이지 이동하면서 탐색끝까지 하던지 그건 나중에 ㄱㄱ
 """
-
